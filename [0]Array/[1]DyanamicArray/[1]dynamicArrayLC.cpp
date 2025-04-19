5509701a7f6adc56c51872d5aa29479fe1b8c18b
sup-neupane <080bct091.suparna@pcampus.edu.np>
2025-04-20 00:19:45 +0545

    Fixed some minor syntax issue

--- [0]Array/[1]DyanamicArray/[1]dynamicArrayLC.cpp
+++ [0]Array/[1]DyanamicArray/[1]dynamicArrayLC.cpp
@@ -3,14 +3,18 @@
 #include <vector>
 using namespace std;
 
-vector<int> getConcatenation(const vector<int>& nums) {
-    int n = nums.size();
-    vector<int> ans(2 * n);
-    
-    for (int i = 0; i < n; i++) {
-        ans[i] = nums[i];      
-        ans[i + n] = nums[i];  
+class Solution {
+    public:
+        vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(2 * n);
+        
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];      
+            ans[i + n] = nums[i];  
+        }
+        
+        return ans;
     }
     
-    return ans;
-}
+    };
