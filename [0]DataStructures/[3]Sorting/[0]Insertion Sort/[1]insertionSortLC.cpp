84391dcc876bb89e11ff4fc6e3e602fbb8174226
sup-neupane <080bct091.suparna@pcampus.edu.np>
2025-04-22 18:02:10 +0545

    Sorting Changes

--- [0]DataStructures/[3]Sorting/[0]Insertion Sort/[1]insertionSortLC.cpp
+++ [0]DataStructures/[3]Sorting/[0]Insertion Sort/[1]insertionSortLC.cpp
@@ -7,7 +7,7 @@ using std::vector;
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        for( int i =0 ; i < nums.size(); i++){
+        for( int i = 1 ; i < nums.size(); i++){
             int j = i -1;
             while ( j>=0 && nums[j + 1] < nums[j] ){
                 int temp = nums[j+1];
--- [0]DataStructures/[3]Sorting/[3]Bucket Sort/[1]bucketSortLC.cpp
+++ [0]DataStructures/[3]Sorting/[3]Bucket Sort/[1]bucketSortLC.cpp
@@ -1,2 +1,26 @@
 // Sort Colors
 // https://leetcode.com/problems/sort-colors/
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        // Assuming nums only contains 0, 1 or 2
+        int counts[] = {0, 0, 0};
+
+        // Count the quantity of each value in nums
+        for (int n : nums) {
+            counts[n]++;
+        }
+
+        int i = 0;
+        for (int n = 0; n < 3; n++) {
+            for (int j = 0; j < counts[n]; j++) {
+                nums[i] = n;
+                i++;
+            }
+        }
+    }
+};
